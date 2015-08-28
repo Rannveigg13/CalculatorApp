65f2a1038fac2279078ec7e3abd6bead1c437b51
Rannveigg13 <rannveigg13@ru.is>
2015-08-28 16:08:10 +0000

    Initial commit

---
+++ app/src/main/java/com/example/notandi/calculatorapp/CalcPreferenceActivity.java
@@ -0,0 +1,16 @@
+package com.example.notandi.calculatorapp;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+/**
+ * Created by Notandi on 28-Aug-15.
+ */
+public class CalcPreferenceActivity extends PreferenceActivity{
+
+    @Override
+    protected  void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState);
+
+    }
+}
